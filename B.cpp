effd9832cb289161014fd0af0faac3f5061f7867
テイイ <148879729+Tetsu45@users.noreply.github.com>
2024-06-15 14:31:39 +0900

    frr

--- B.cpp
+++ B.cpp
@@ -1,3 +1,23 @@
 #include <iostream>
 using namespace std;
-s
+int selection_sort(int* arr,int *size){
+    for(int ix= 0;ix<*size;ix++){
+        int minimum = arr[ix];
+        for(int jx= i+1;
+    }
+
+}
+bool read_input(){
+    int size = 0;
+    std:: cin >> size;
+    if (size < 1 || size > 100) return false;
+    int* arr= new int[size];
+    for(int i= 0;i<size;i++){
+        std::cin>>arr[i];
+    }
+
+
+}
+int main(){
+    return 0;
+}
